a27903f69fa257e98a7ce731e799aafe9e15eb56
Michael Nien <mobile.michael.nien@gmail.com>
2023-11-07 20:57:14 +0800

    新增 GitHubActions 和 vuepress

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  title: 'Android',
+  description: 'Android',
+  base: '/Android/',
+  repo: 'https://github.com/MichaelNien/Android',
+  plugins: [
+	'vuepress-plugin-mermaidjs'
+  ],
+  themeConfig: {
+    sidebarDepth: 0,
+    sidebar: [
+      ['/', '首頁'],
+    ],
+    nav: [
+    ]
+  }
+}
